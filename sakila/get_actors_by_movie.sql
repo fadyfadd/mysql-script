cfbc4e77c5ca13cbecbe444252cf7496302870d0
fadyfaddoul <fady_faddoul@hotmail.com>
2023-10-08 17:59:19 +0300

    cleanup/fixes

--- sakila/get_actors_by_movie.sql
+++
@@ -1,8 +0,0 @@
-use sakila;
-drop procedure if exists get_actors_by_movie;
-DELIMITER $$
-create procedure get_actors_by_movie(amovie_id int , out actor_count int , out f int)
-begin
-		set actor_count = 0;   
-end$$
-DELIMITER ;
--- sakila/get_movies_by_release_year.sql
+++ sakila/get_movies_by_release_year.sql
@@ -1,7 +1,7 @@
 use sakila;
 drop procedure if exists get_movies_by_release_year;
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `get_movies_by_release_year`(arelease_year int , out movie_count int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_movies_by_release_year`(arelease_year int)
 begin
 	select film_id , title , description , release_year from film
     where arelease_year is null or release_year = arelease_year order by release_year desc;
