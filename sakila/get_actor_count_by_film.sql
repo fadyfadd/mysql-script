48dc783703ea31bc43342edae95b7e66b583c6f3
fadyfaddoul <fady_faddoul@hotmail.com>
2023-10-11 20:57:42 +0300

    stored procedures modifications

--- sakila/get_actor_count_by_film.sql
+++ sakila/get_actor_count_by_film.sql
@@ -1,8 +1,8 @@
 use sakila;
 drop procedure if exists get_actor_count_by_film;
 DELIMITER $$
-CREATE DEFINER=`root`@`localhost` PROCEDURE `get_actor_count_by_film`(amovie_id int , out movie_count int)
+CREATE DEFINER=`root`@`localhost` PROCEDURE `get_actor_count_by_film`(afilm_id int , out film_count int)
 begin
-	select count(*) into movie_count from film_actor where film_id = amovie_id or amovie_id is null;    
+	select count(*) into film_count from film_actor where film_id = afilm_id or afilm_id is null;    
 end$$
 DELIMITER ;
--- sakila/get_inventory_by_film.sql
+++ sakila/get_inventory_by_film.sql
@@ -1,16 +1,16 @@
 use sakila;
 drop procedure if exists get_inventory_by_film;
 DELIMITER $$
-create procedure get_inventory_by_film(out movie_count int , amovie_id int , out random_value numeric(10,8))
+create procedure get_inventory_by_film(out film_count int , afilm_id int , out random_value numeric(10,8))
 begin
 		  set random_value = rand();
 		  SELECT COUNT(*)
-		  into movie_count from inventory where amovie_id is null or film_id = amovie_id;
+		  into film_count from inventory where afilm_id is null or film_id = afilm_id;
 		  
           select film.film_id , film.title , count(*) as count from inventory
           inner join film
           on film.film_id = inventory.film_id
-          where amovie_id is null or film.film_id = amovie_id
+          where afilm_id is null or film.film_id = afilm_id
           group by film.film_id , film.title;  
 end$$
 DELIMITER ;
